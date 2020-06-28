dec8ebd89b0a9d2c3f788220031918d07966a897
hijera <hi-jera@yandex.ru>
2020-06-28 22:58:13 +0300

    Форматирование

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ import ForzaPage from "./components/ForzaPage";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
-    const menu_list=[{link:'/',title:'Главная'},
-        {link:'/drift',title:'Дрифт-такси'},
-        {link:'/timeattack',title:'Time Attack'},
-        {link:'/forza',title:'Forza Karting'}];
-  return (
-      <Router>
-        <div>
-          <Menu menu={menu_list} />
-          <div className="page">
-            <Route path="/" exact component={HomePage} />
-            <Route path="/drift" component={DriftPage} />
-            <Route path="/timeattack" component={TimeAttackPage} />
-            <Route path="/forza" component={ForzaPage} />
-          </div>
-        </div>
-      </Router>
-  );
+    const menu_list = [{link: '/', title: 'Главная'},
+        {link: '/drift', title: 'Дрифт-такси'},
+        {link: '/timeattack', title: 'Time Attack'},
+        {link: '/forza', title: 'Forza Karting'}];
+    return (
+        <Router>
+            <div>
+                <Menu menu={menu_list}/>
+                <div className="page">
+                    <Route path="/" exact component={HomePage}/>
+                    <Route path="/drift" component={DriftPage}/>
+                    <Route path="/timeattack" component={TimeAttackPage}/>
+                    <Route path="/forza" component={ForzaPage}/>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
